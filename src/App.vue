8f27afd053fb3f507689dd2b1c8e1314cce8ed96
Vick Kumar <vick.kumar44@gmail.com>
2017-11-15 10:22:48 -0500

    initialized project

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1>CLT OPS</h1>
+    <p>{{data}}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      data: {}
+    }
+  },
+  methods: {
+    async getData() {
+      this.data = await axios.get('../config/data.json')
+    }
+  },
+  created() {
+    this.getData();
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
